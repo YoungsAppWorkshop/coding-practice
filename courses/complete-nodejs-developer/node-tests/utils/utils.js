c43bcf401bf4ac29d1b0a65fffe284c6eb61dc13
YongJe Joung <ryviusm6@gmail.com>
2018-11-12 17:57:19 +0900

    Add Node test from complete node.js development course

--- courses/complete-nodejs-developer/node-tests/utils/utils.js
+++ courses/complete-nodejs-developer/node-tests/utils/utils.js
@@ -1,2 +1,22 @@
-module.exports.add = (a, b) => a + b;
-module.exports.square = (x) => x * x;
+module.exports.add = (a, b) => a + b
+
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b)
+  }, 100)
+}
+
+module.exports.square = a => a * a
+
+module.exports.asyncSquare = (a, callback) => {
+  setTimeout(() => {
+    callback(a * a)
+  }, 100)
+}
+
+module.exports.setName = (user, fullName) => {
+  const names = fullName.split(' ')
+  user.firstName = names[0]
+  user.lastName = names[1]
+  return user
+}
--- courses/complete-nodejs-developer/node-tests/utils/utils.test.js
+++ courses/complete-nodejs-developer/node-tests/utils/utils.test.js
@@ -1,15 +1,61 @@
+const expect = require('expect')
+
 const utils = require('./utils')
 
-it('should add two numbers', () => {
-  const res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`);
-  }
-});
+describe('Utils', () => {
 
-it('should square a number', () => {
-  const res = utils.square(3);
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}.`);
-  }
-});
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      const res = utils.add(33, 11)
+      expect(res).toBe(44).toBeA('number')
+      // if (res !== 44) {
+      //   throw new Error(`Expected 44, but got ${res}.`)
+      // }
+    })
+  })
+
+
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, sum => {
+      expect(sum).toBe(7).toBeA('number')
+      done()
+    })
+  })
+
+  it('should square a number', () => {
+    const res = utils.square(4)
+    expect(res).toBe(16).toBeA('number')
+  })
+
+  it('should async square a number', (done) => {
+    utils.asyncSquare(4, square => {
+      expect(square).toBe(16).toBeA('number')
+      done()
+    })
+  })
+
+  it('should set first and last names', () => {
+    const user = { location: 'Philadelphia', age: 25 }
+    const res = utils.setName(user, 'Andrew Mead')
+    expect(res).toInclude({
+      firstName: 'Andrew',
+      lastName: 'Mead'
+    }).toBeA('object')
+  })
+
+
+  // it('should expect some values', () => {
+  //   // expect(12).toBe(12)
+  //   expect({name: 'Andrew'}).toEqual({name: 'Andrew'})
+  //   expect([2, 3, 4]).toInclude(3)
+  //   expect([2, 3, 4]).toExclude(1)
+  //   expect({
+  //     name: 'Andrew',
+  //     age: 25,
+  //     location: 'Philadelphia'
+  //   }).toInclude({
+  //     age: 25
+  //   })
+  // })
+
+})
